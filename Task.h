bbe1c2dd8b0e4e1cddceab8f01145fbd7f693145
GBluca <gianluca.ble.19@gmail.com>
2025-05-05 23:15:55 +0200

    create classi Task.h e TaskManager.h, definiti metodi e attributi classe Task.h

---
+++ Task.h
@@ -0,0 +1,43 @@
+//
+// Created by gbluc on 05/05/2025.
+//
+
+#ifndef TASK_H
+#define TASK_H
+#include <string>
+
+
+class Task {
+
+    std::string description;
+    bool completed;
+
+    Task() {
+        completed = false;
+    }
+
+    explicit Task(const std::string &description) {
+        this->description += description;
+        completed = false;
+    }
+
+    void marlCompleted() {
+        completed = true;
+    }
+
+    std::string getDescription() const {
+        return  this->description;
+    }
+
+    bool isCompleted() const {
+        return completed;
+    }
+
+
+
+
+};
+
+
+
+#endif //TASK_H
---
+++ TaskManager.h
@@ -0,0 +1,16 @@
+//
+// Created by gbluc on 05/05/2025.
+//
+
+#ifndef TASKMANAGER_H
+#define TASKMANAGER_H
+
+
+
+class TaskManager {
+
+};
+
+
+
+#endif //TASKMANAGER_H
